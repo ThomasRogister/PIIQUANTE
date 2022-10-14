7d903bf3e9148d19a5ddc35eb48060870e5bb3d2
thomas.rogister <thomasrogister@protonmail.com>
2022-10-14 18:51:49 +0200

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorizatuin.split(' ')[1];
+        const decodedToken = jwt.verify(token, '9hF8-LOBuYv%aZ3VduvA-bR#Ez');
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
+    } catch (error) {
+        res.status(401).json({ error });
+    }
+
+}
